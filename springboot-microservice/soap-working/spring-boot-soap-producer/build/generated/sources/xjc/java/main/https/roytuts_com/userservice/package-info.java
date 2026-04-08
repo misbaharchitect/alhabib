8de98b84518187d3075ca48191b01a0a9fec05da
ubuntu <misbah.architect@gmail.com>
2026-04-08 20:42:50 +0530

    gitignore

--- springboot-microservice/soap-working/spring-boot-soap-producer/build/generated/sources/xjc/java/main/https/roytuts_com/userservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "https://roytuts.com/UserService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package https.roytuts_com.userservice;
